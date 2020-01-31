d5c2fee0431f8a6aee4fd21b5adbdbeabcabeaf5
samreid <reids@colorado.edu>
2020-01-31 08:51:15 -0700

    Update slider enabledRange phetioDocumentation, see https://github.com/phetsims/gravity-and-orbits/issues/293

--- js/phet-io/gases-intro-phet-io-elements-baseline.js
+++ js/phet-io/gases-intro-phet-io-elements-baseline.js
@@ -4136,7 +4136,7 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioTypeName": "PropertyIO<BooleanIO>"
     },
     "gasesIntro.introScreen.view.heaterCoolerNode.slider.enabledRangeProperty": {
-      "phetioDocumentation": "",
+      "phetioDocumentation": "Sliders support two ranges: the outer range which specifies the min and max of the track and the enabledRangeProperty, which determines how low and high the thumb can be dragged within the track.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
       "phetioFeatured": false,
@@ -6632,7 +6632,7 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioTypeName": "PropertyIO<BooleanIO>"
     },
     "gasesIntro.lawsScreen.view.heaterCoolerNode.slider.enabledRangeProperty": {
-      "phetioDocumentation": "",
+      "phetioDocumentation": "Sliders support two ranges: the outer range which specifies the min and max of the track and the enabledRangeProperty, which determines how low and high the thumb can be dragged within the track.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
       "phetioFeatured": false,
