47e6ddab541ef06e9a6f1f1b307f2a0aa4fc8e09
chrisklus <christopher.klusendorf@colorado.edu>
2020-04-06 13:32:00 -0400

    Update phet-io files, see https://github.com/phetsims/joist/issues/597

--- js/phet-io/gases-intro-phet-io-elements-baseline.js
+++ js/phet-io/gases-intro-phet-io-elements-baseline.js
@@ -2511,10 +2511,10 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioTypeName": "PropertyIO<ScreenIO>"
     },
     "gasesIntro.homeScreen.nameProperty": {
-      "phetioDocumentation": "",
+      "phetioDocumentation": "The name of the screen. Changing this value will update the screen name for the screen's corresponding button on the navigation bar and home screen, if they exist. May be null for single-screen simulations.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
-      "phetioFeatured": false,
+      "phetioFeatured": true,
       "phetioHighFrequency": false,
       "phetioIsArchetype": false,
       "phetioPlayback": false,
@@ -3356,10 +3356,10 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioTypeName": "PropertyIO<EnumerationIO(FAST|NORMAL|SLOW)>"
     },
     "gasesIntro.introScreen.nameProperty": {
-      "phetioDocumentation": "",
+      "phetioDocumentation": "The name of the screen. Changing this value will update the screen name for the screen's corresponding button on the navigation bar and home screen, if they exist. May be null for single-screen simulations.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
-      "phetioFeatured": false,
+      "phetioFeatured": true,
       "phetioHighFrequency": false,
       "phetioIsArchetype": false,
       "phetioPlayback": false,
@@ -8894,10 +8894,10 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioTypeName": "PropertyIO<EnumerationIO(FAST|NORMAL|SLOW)>"
     },
     "gasesIntro.lawsScreen.nameProperty": {
-      "phetioDocumentation": "",
+      "phetioDocumentation": "The name of the screen. Changing this value will update the screen name for the screen's corresponding button on the navigation bar and home screen, if they exist. May be null for single-screen simulations.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
-      "phetioFeatured": false,
+      "phetioFeatured": true,
       "phetioHighFrequency": false,
       "phetioIsArchetype": false,
       "phetioPlayback": false,
