0fb3871289e2dc967441919cceb46e736738351c
Jesse <jesse.greenberg@colorado.edu>
2022-09-01 16:31:14 -0400

    Rename general -> simulation in many places, see https://github.com/phetsims/joist/issues/838

--- js/gases-intro-main.js
+++ js/gases-intro-main.js
@@ -26,7 +26,7 @@ const simOptions = {
     visualOptions: {
       supportsProjectorMode: true
     },
-    generalOptions: {
+    simulationOptions: {
       customPreferences: [ {
         createContent: tandem => new GasPropertiesPreferencesNode( {
           tandem: tandem.createTandem( 'simPreferences' )
